c5bdc2a7663c64114c44ed330b1af879637a3e84
Julien <julien@powerinbox.com>
2012-03-20 15:25:30 -0400

    Add support for running under partial trust

--- src/mongodb/Properties/AssemblyInfo.cs
+++ src/mongodb/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+[assembly: AllowPartiallyTrustedCallers]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
